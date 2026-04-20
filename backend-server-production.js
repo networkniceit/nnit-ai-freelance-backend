3471cfd897269a9fcd567086c37c227a3aac3a9f
networkniceit <networkniceit@gmail.com>
2026-04-20 23:49:59 +0200

    Fix port to 8080

--- backend-server-production.js
+++ backend-server-production.js
@@ -261,7 +261,7 @@ app.use((err, req, res, next) => {
 
 // ==================== SERVER STARTUP ====================
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 const startServer = async () => {
   try {
