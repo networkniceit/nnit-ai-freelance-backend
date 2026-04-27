cdf4dc68db2e7519f6ce6c38555ed93bde947e1e
networkniceit <networkniceit@gmail.com>
2026-04-27 19:31:06 +0200

    fix: use port 465 for SMTP

--- services/emailService.js
+++ services/emailService.js
@@ -1,9 +1,8 @@
-const nodemailer = require('nodemailer');
-
+const nodemailer = require('nodemailer'); 
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
-  port: process.env.SMTP_PORT,
-  secure: false,
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
